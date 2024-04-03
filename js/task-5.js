f835f65e97545687a1f211eba36c69d39d236f5a
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-03 17:30:39 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -77,17 +77,20 @@
 
 // }
 
-fetch("<https://jsonplaceholder.typicode.com/users>")
+const options = {
+	method: "GET"
+};
+
+fetch("<https://jsonplaceholder.typicode.com/users>", options)
   .then(response => {
-      // Response handling
-      console.log(response);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
   })
   .then(data => {
-      // Data handling
-      console.log(data);
+    // Data handling
   })
   .catch(error => {
-      // Error handling
-      console.log(error);
-  }); 
-  
+    // Error handling
+  });
