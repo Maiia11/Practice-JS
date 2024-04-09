e6eb5171dcdf80b7c202616b0a69fd6a3a640aab
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-09 21:50:34 +0300

    а

--- js/task-5.js
+++ js/task-5.js
@@ -77,20 +77,19 @@
 
 // }
 
-const options = {
-	method: "GET"
-};
 
-fetch("<https://jsonplaceholder.typicode.com/users>", options)
-  .then(response => {
+const postIdToDelete = 1;
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+  method: "DELETE",
+})
+  .then((response) => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
   })
-  .then(data => {
-    // Data handling
-  })
-  .catch(error => {
-    // Error handling
-  });
+  .then((deletedPost) => console.log(deletedPost))
+  .catch(error => console.log("Error:", error));
+
+
